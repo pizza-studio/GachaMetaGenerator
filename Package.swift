85d90d130bdf778467032989b70baf3b8d11d17b
ShikiSuen <shikisuen@outlook.com>
2024-07-23 16:54:15 +0800

    Add supported iOS & watchOS platform versions.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GachaMetaGenerator",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
